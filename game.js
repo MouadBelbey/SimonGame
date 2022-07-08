eb52cb2ad61072b990605db516bdfea6721080e2
Mouad Belbey <104021030+MouadBelbey@users.noreply.github.com>
2022-07-08 17:04:28 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,91 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(gamePattern.length - 1);
+});
+
+
+
+$("body").keydown(function() {
+
+  if (!started) {
+
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+})
+
+
+function nextSequence() {
+
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
+
+}
+
+
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    if (gamePattern.length === userClickedPattern.length) {
+
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  }
+
+  for (var i = 0; i < userClickedPattern.length; i++) {
+    if (gamePattern[i] !== userClickedPattern[i]) {
+
+      playSound("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function() {
+        $("body").removeClass("game-over")
+      }, 200);
+      $("#level-title").text("Game Over, Press Any Key To Restart")
+      starOver();
+    }
+  }
+
+}
+
+function playSound(name) {
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+}
+
+function starOver() {
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+  started = false;
+}
